5a8ddc43ee05c09df39d19a29164cdeb74924a56
bulutyelken <190101052@ogrenci.yalova.edu.tr>
2022-07-23 21:52:53 +0300

    bg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,10 @@
-import Logo from '../assets/bulutlogo.png'
-import {FaBars, FaTimes} from 'react-icons/fa'
+//import Logo from '../assets/bulutlogo.png'
+
 
 function Header(){
     return(
-        <div className='fixed w-full h-24 flex items-center justify-between px-4 bg-[#E7AB79]'>
-            <div className="flex items-center">
-                <img src={Logo} alt="Logo" className='h-20'/>   
-            </div>
+        <div className="">
 
-            {/*menu*/}
-            <div>
-                <ul className='flex'>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Skills</li>
-                    <li>Works</li>
-                    <li>Contact</li>
-                </ul>
-            </div>
-
-            {/*Hamburger */}
-            <div className='hidden'><FaBars /></div>
-
-            {/*Mobile Menu*/}
-            <ul className='hidden'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Works</li>
-                <li>Contact</li>
-            </ul>
-
-            {/*social icons*/}
-            <div className='hidden'></div>
         </div>
     )
 }
